26b4b355c05db72041091453893e56720037bbbc
Novo <120156959+Novo1999@users.noreply.github.com>
2023-04-07 14:29:34 +0600

    ok

--- script.js
+++ script.js
@@ -6,23 +6,37 @@ loginBtn.addEventListener('click', function(){
     loginArea.style.display = 'none';
     const transactionBtn = document.getElementById('transaction-area');
     transactionBtn.style.display = 'block';
-});
+})
+//deposit button event handler
 const depositBtn = document.getElementById('addDeposit');
 depositBtn.addEventListener('click', function(){
     const depositAmount = document.getElementById('depositAmount').value;
     const depositNumber = parseFloat(depositAmount);
     
-    const currentDeposit = document.getElementById('currentDeposit').innerText;
-    const currentDepositNumber = parseFloat(currentDeposit);
-    const totalDeposit = depositNumber + currentDepositNumber;
-    
-    document.getElementById('currentDeposit').innerText = totalDeposit;
-    document.getElementById('depositAmount').value = '';
+    updateSpanText('currentDeposit', depositNumber);
+    updateSpanText("currentBalance", depositNumber);
 
-    const currentBalance = document.getElementById('currentBalance').innerText;
-    const totalBalanceNumber = parseFloat(currentBalance) + depositNumber;
-    document.getElementById('currentBalance').innerText = totalBalanceNumber;
+    document.getElementById('depositAmount').value = '';
 })
+function updateSpanText(id, depositNumber){
+    const current = document.getElementById(id).innerText;
+    const totalAmount = parseFloat(current) + depositNumber;
+    document.getElementById(id).innerText = totalAmount;
+}
+
+//withdraw button event handler
+const withdrawBtn = document.getElementById('addWithdraw');
+withdrawBtn.addEventListener('click', function () {
+    const withdrawAmount = document.getElementById('withdrawAmount').value;
+    const withdrawNumber = parseFloat(withdrawAmount);
+
+    const currentWithdraw = document.getElementById('currentWithdraw').innerText;
+    const totalWithdraw = withdrawNumber + parseFloat(currentWithdraw);
+    
+    document.getElementById('currentWithdraw').innerText = totalWithdraw;
+    
+})
+
 
 
 
